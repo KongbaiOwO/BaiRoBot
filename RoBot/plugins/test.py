1960ed035a6babba31e7e6c5df99b17a74550853
空白白 <99713809+KongbaiOwO@users.noreply.github.com>
2022-07-20 23:03:46 +0800

    测试模块，用于检测机器人是否运行

--- RoBot/plugins/test.py
+++ RoBot/plugins/test.py
@@ -1,4 +1,4 @@
-#测试插件，用于检测机器人是否运行
+#测试插件，用于检测机器人是否运行 
 def run(platform,type,uid,gid,msg,BOTQQ):
     if type=='group' and f'[CQ:at,qq={BOTQQ}]' in msg and '在吗' in msg:
         platform.sendmsg(type, gid, '我在')
